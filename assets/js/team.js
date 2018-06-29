15ac595d7eb52046af6029bf88ea701b43387f3f
naveennvrgup <naveennvrgup@gmail.com>
2018-06-29 13:49:31 +0530

    start of the team

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,33 +1,33 @@
+var team = document.querySelector('#team .d-flex');
+
 var teamMember = `
-    <div class="col-6 col-sm-6 col-xs-6 col-md-6 col-lg-4 nopad">
-        <div class="team-item wow fadeInUp" data-wow-delay="0.2s">
-            <div class="team-img">
-                <img class="img-fluid" src="assets/img/team/team-01.jpg" alt="">
-                <div class="team-overlay">
-                <div class="overlay-social-icon text-center">
-                    <ul class="social-icons">
-                    <li>
-                        <a href="#">
-                        <i class="lni-facebook-filled" aria-hidden="true"></i>
-                        </a>
-                    </li>
-                    <!-- <li><a href="#"><i class="lni-twitter-filled" aria-hidden="true"></i></a></li> -->
-                    <li>
-                        <a href="#">
-                        <i class="lni-linkedin-filled" aria-hidden="true"></i>
-                        </a>
-                    </li>
-                    <!-- <li><a href="#"><i class="lni-behance" aria-hidden="true"></i></a></li> -->
-                    </ul>
-                </div>
-                </div>
-            </div>
-            <div class="info-text">
-                <h3>
-                <a href="#">JONATHON DOE</a>
-                </h3>
-                <p>Product Designer</p>
-            </div>
+<div class="team-item wow fadeInUp" data-wow-delay="1.2s">
+    <div class="team-img">
+        <img class="img-fluid" src="assets/img/team300/00100sPORTRAIT_00100_BURST20180426164356867_COVER - Anuj Soni.jpg" alt="">
+    <div class="team-overlay">
+        <div class="overlay-social-icon text-center">
+        <ul class="social-icons">
+            <li>
+            <a href="#">
+                <i class="lni-facebook-filled" aria-hidden="true"></i>
+            </a>
+            </li>
+            <!-- <li><a href="#"><i class="lni-twitter-filled" aria-hidden="true"></i></a></li> -->
+            <li>
+            <a href="#">
+                <i class="lni-linkedin-filled" aria-hidden="true"></i>
+            </a>
+            </li>
+            <!-- <li><a href="#"><i class="lni-behance" aria-hidden="true"></i></a></li> -->
+        </ul>
         </div>
     </div>
+    </div>
+    <div class="info-text">
+        <h3> Chris Adams </h3>
+    </div>
+</div>
 `;
+
+console.log(teamInfo);
+team.innerHTML += teamMember;
