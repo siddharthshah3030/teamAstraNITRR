2524884611990e90fe5e32ad78fc7d7a38b9c028
naveennvrgup <naveennvrgup@gmail.com>
2018-06-29 17:05:27 +0530

    basic photos added

--- assets/js/team.js
+++ assets/js/team.js
@@ -5,33 +5,24 @@ teamInfo.forEach((member) => {
     team[0].innerHTML +=`
         <div class="team-item wow fadeInUp" data-wow-delay="1.2s">
             <div class="team-img">
-                <img class="img-fluid" src=${member.img} alt="">
+                <img class="img-fluid" src="./assets/img/team300/${member.name.toLowerCase()}.jpg" alt="">
             <div class="team-overlay">
                 <div class="overlay-social-icon text-center">
                 <ul class="social-icons">
                     <li>
                     <a href="#">
-                        <i class="lni-facebook-filled" aria-hidden="true"></i>
+                        <i class="lni-link" aria-hidden="true"></i>
                     </a>
                     </li>
-                    <!-- <li><a href="#"><i class="lni-twitter-filled" aria-hidden="true"></i></a></li> -->
-                    <li>
-                    <a href="#">
-                        <i class="lni-linkedin-filled" aria-hidden="true"></i>
-                    </a>
-                    </li>
-                    <!-- <li><a href="#"><i class="lni-behance" aria-hidden="true"></i></a></li> -->
                 </ul>
                 </div>
             </div>
             </div>
             <div class="info-text">
-                <h3>${member.name}</h3>
+                <h3>${member.name.toLowerCase()}</h3>
             </div>
         </div>
         `;
-    
-
 })
 
 // console.log(team[0].innerHTML);
