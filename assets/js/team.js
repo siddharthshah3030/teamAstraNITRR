9e12da384bb266d060bd5921a59d8fc91420496e
naveennvrgup <naveennvrgup@gmail.com>
2018-06-29 16:35:29 +0530

    named img files

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,33 +1,37 @@
-var team = document.querySelector('#team .d-flex');
+var team = $('.members');
 
-var teamMember = `
-<div class="team-item wow fadeInUp" data-wow-delay="1.2s">
-    <div class="team-img">
-        <img class="img-fluid" src="assets/img/team300/00100sPORTRAIT_00100_BURST20180426164356867_COVER - Anuj Soni.jpg" alt="">
-    <div class="team-overlay">
-        <div class="overlay-social-icon text-center">
-        <ul class="social-icons">
-            <li>
-            <a href="#">
-                <i class="lni-facebook-filled" aria-hidden="true"></i>
-            </a>
-            </li>
-            <!-- <li><a href="#"><i class="lni-twitter-filled" aria-hidden="true"></i></a></li> -->
-            <li>
-            <a href="#">
-                <i class="lni-linkedin-filled" aria-hidden="true"></i>
-            </a>
-            </li>
-            <!-- <li><a href="#"><i class="lni-behance" aria-hidden="true"></i></a></li> -->
-        </ul>
+teamInfo.forEach((member) => {
+    console.log(member.img);
+    team[0].innerHTML +=`
+        <div class="team-item wow fadeInUp" data-wow-delay="1.2s">
+            <div class="team-img">
+                <img class="img-fluid" src=${member.img} alt="">
+            <div class="team-overlay">
+                <div class="overlay-social-icon text-center">
+                <ul class="social-icons">
+                    <li>
+                    <a href="#">
+                        <i class="lni-facebook-filled" aria-hidden="true"></i>
+                    </a>
+                    </li>
+                    <!-- <li><a href="#"><i class="lni-twitter-filled" aria-hidden="true"></i></a></li> -->
+                    <li>
+                    <a href="#">
+                        <i class="lni-linkedin-filled" aria-hidden="true"></i>
+                    </a>
+                    </li>
+                    <!-- <li><a href="#"><i class="lni-behance" aria-hidden="true"></i></a></li> -->
+                </ul>
+                </div>
+            </div>
+            </div>
+            <div class="info-text">
+                <h3>${member.name}</h3>
+            </div>
         </div>
-    </div>
-    </div>
-    <div class="info-text">
-        <h3> Chris Adams </h3>
-    </div>
-</div>
-`;
+        `;
+    
 
-console.log(teamInfo);
-team.innerHTML += teamMember;
+})
+
+// console.log(team[0].innerHTML);
