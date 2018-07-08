c9bf87365211a43fc0c7b38b019022d59d76bc08
naveennvrgup <naveennvrgup@gmail.com>
2018-07-08 21:06:55 +0530

    reverting git changes

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -13,10 +13,10 @@ deselectImgBtn.addEventListener('click', function () {
 allGalleryImgs.forEach(function (img) {
     img.addEventListener('click', function () {
         // updating img file with crisp img
-        var new_src = img.src.split('/');
-        new_src[5] = 'astra-gallery-crisp';
-        new_src= new_src.join('/')
-        img.src = new_src;
+        // var new_src = img.src.split('/');
+        // new_src[5] = 'astra-gallery-crisp';
+        // new_src= new_src.join('/')
+        // img.src = new_src;
         // showing the enlarged img
         containerForSelectedImg.style.height = '100%';
         enlargedSelectedImage.src = img.src;
